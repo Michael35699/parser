6fe20cecd391b12e2d696621f758630499f6bde4
Michaelz35699 <michaelz35699@gmail.com>
2022-04-12 18:39:39 +0800

    Added new `Predicate` methods

--- lib/src/util/classes/predicate.dart
+++ lib/src/util/classes/predicate.dart
@@ -1,6 +1,9 @@
 import "package:parser_peg/util.dart";
 
 class Predicate {
+  static GenericPredicate<T> tautology<T>() => (T item) => true;
+  static GenericPredicate<T> contradiction<T>() => (T item) => false;
+
   static GenericPredicate<T> not<T>(bool Function(T) fn) => (T item) => !fn(item);
   static GenericPredicate<T> or<T>(GenericPredicate<T> left, GenericPredicate<T> right) =>
       (T item) => left(item) || right(item);
