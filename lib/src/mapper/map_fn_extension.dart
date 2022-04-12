e56bc698e2148500f3cb20c0c9d4520717bc55ef
Michaelz35699 <michaelz35699@gmail.com>
2022-04-13 04:28:50 +0800

    Minor changes

--- lib/src/mapper/map_fn_extension.dart
+++ lib/src/mapper/map_fn_extension.dart
@@ -1,5 +1,5 @@
 import "package:parser_peg/internal_all.dart";
 
 extension MapFunctionExtension on MapFunction {
-  MapFunction operator <<(Function fn) => (dynamic r, Context c) => Function.apply(fn, <dynamic>[this(r, c)]);
+  MapFunction operator >>>(Function fn) => (dynamic r, Context c) => Function.apply(fn, <dynamic>[this(r, c)]);
 }
--- lib/src/parser/synthesized/complex/json/json.dart
+++ lib/src/parser/synthesized/complex/json/json.dart
@@ -4,7 +4,7 @@ typedef JsonEntry = MapEntry<String, Object>;
 typedef JsonMap = Map<String, Object>;
 typedef JsonList = List<Object>;
 
-Parser jsonParser() => _value.end();
+Parser parser() => _value.end();
 Parser _value() => _valueBody.trim();
 Parser _valueBody() => _array | _object | jsonNumber | _string | _trueValue | _falseValue | _nullValue;
 
@@ -13,7 +13,7 @@ Parser _elements() => _value % -",".t | success(JsonList.empty());
 
 Parser _object() => "{" >> _members.trim() << "}";
 Parser _members() =>
-    _pair % -",".t ^ $list<JsonEntry>() ^ $type(JsonMap.fromEntries) | //
+    _pair % -",".t ^ $list<JsonEntry>() >>> JsonMap.fromEntries | //
     success(JsonMap.identity());
 
 Parser _pair() => _string & -":".t & _value ^ $2(JsonEntry.new);
