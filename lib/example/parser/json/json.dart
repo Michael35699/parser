484d2b77332b61c98a4e58c0a3ae4e1962368fb2
Michaelz35699 <michaelz35699@gmail.com>
2022-04-13 17:44:29 +0800

    Minor changes

--- lib/example/parser/json/json.dart
+++ lib/example/parser/json/json.dart
@@ -1,22 +1,22 @@
 import "package:parser_peg/parser_peg.dart";
 
-typedef JsonEntry = MapEntry<String, Object>;
-typedef JsonMap = Map<String, Object>;
-typedef JsonList = List<Object>;
+typedef _JsonEntry = MapEntry<String, Object?>;
+typedef _JsonMap = Map<String, Object?>;
+typedef _JsonList = List<Object?>;
 
 Parser jsonParser() => _value.end();
 Parser _value() => _valueBody.trim();
 Parser _valueBody() => _array | _object | jsonNumber | _string | _trueValue | _falseValue | _nullValue;
 
 Parser _array() => "[" >> _elements.trim() << "]";
-Parser _elements() => _value % -",".t | success(JsonList.empty());
+Parser _elements() => _value % -",".t ^ $list<_JsonList>() | success(_JsonList.empty());
 
 Parser _object() => "{" >> _members.trim() << "}";
 Parser _members() =>
-    _pair % -",".t ^ $list<JsonEntry>() >>> JsonMap.fromEntries | //
-    success(JsonMap.identity());
+    _pair % -",".t ^ $list<_JsonEntry>() >>> _JsonMap.fromEntries | //
+    success(_JsonMap.identity());
 
-Parser _pair() => _string & -":".t & _value ^ $2(JsonEntry.new);
+Parser _pair() => _string & -":".t & _value ^ $2(_JsonEntry.new);
 
 Parser _string() => jsonString().$at(1);
 
