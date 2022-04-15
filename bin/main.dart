0fe1f08074389960212fd8954ad2eba477d9465e
Michaelz35699 <michaelz35699@gmail.com>
2022-04-15 14:31:46 +0800

    `main` changes

--- bin/main.dart
+++ bin/main.dart
@@ -5,32 +5,13 @@ import "package:parser/util.dart";
 
 part "utils.dart";
 
-Parser math() => addition.$;
-Parser addition() =>
-    addition & "+".t & multiplication ^ $3((num l, _, num r) => l + r) | //
-    addition & "-".t & multiplication ^ $3((num l, _, num r) => l - r) |
-    multiplication;
-Parser multiplication() =>
-    multiplication & "*".t & negative ^ $3((num l, _, num r) => l * r) | //
-    multiplication & "/".t & negative ^ $3((num l, _, num r) => l / r) |
-    negative;
-Parser negative() =>
-    "-".t & negative ^ $2((_, num v) => -v) | //
-    power;
-Parser power() =>
-    atom & "^".t & power ^ $3((num l, _, num r) => pow(l, r)) | //
-    atom;
-Parser atom() =>
-    digit.plus() ^ $join() >>> int.parse | //
-    "(".t & addition & ")".t ^ $at(1);
+Parser math() =>
+    1.$ ^ math[0] & -"^".t & math[1] ^ $2(pow) | //
+    0.$ ^ number;
 
 void main() {
-  const String input = "1 + 2 * 3";
-  Parser mapped = math.build();
-  Parser unmapped = math.unmapped.build();
-
-  print(mapped.gll(input).toList());
-  print(mapped.peg(input));
-  print(unmapped.gll(input).toList());
-  print(unmapped.peg(input));
+  const String input = "1^2^3";
+  Parser parser = math.undropped.unmapped.build();
+  print(parser.gll(input).toList());
+  print(parser.peg(input));
 }
