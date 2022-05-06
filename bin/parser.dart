fc4d01c6dd8435740bbc9d6d60d33c541ba9251e
Michaelz35699 <michaelz35699@gmail.com>
2022-05-06 14:07:53 +0800

    Minor changes

--- bin/parser.dart
+++ bin/parser.dart
@@ -1,18 +1,36 @@
 // import "package:parser/src/util/shared/time.dart";
 // import "package:parser/example/parser/math.dart" as math_parser;
 import "package:parser/parser.dart" as parser;
+import "package:parser/src/util/shared/time.dart";
 
 int counter = 0;
 
 typedef Parser = parser.Parser;
 
-Parser S() => S & S | "s"
+Parser peg() => "s".plus();
+Parser explicitLeft() => explicitLeft & "s" | "s";
+Parser implicitLeft() => implicitLeft & implicitLeft | "s"
   ..left();
+Parser explicitRight() => "s" & explicitRight | "s";
+Parser implicitRight() => implicitRight & implicitRight | "s"
+  ..right();
 
 void main() {
-  Parser built = S.build();
-  print(Parser.isRightRecursive(built));
-  print(Parser.isDefinitelyRightRecursive(built));
-  print(built.packrat("ssssss"));
-  // print(built.packrat("1 2 3 + +"));
+  const int count = 50;
+  const String input = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+      "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
+
+  for (MapEntry<String, Parser> entry in <String, Parser>{
+    "peg": parser.thunk(peg),
+    "explicitLeft": parser.thunk(explicitLeft),
+    "implicitLeft": parser.thunk(implicitLeft),
+    "explicitRight": parser.thunk(explicitRight),
+    "implicitRight": parser.thunk(implicitRight),
+  }.entries) {
+    print("----- ${entry.key} -----");
+    time(count: count, name: "Pure-left", () => entry.value.peg.left(input));
+    time(count: count, name: "Packrat-Linear", () => entry.value.packrat.linear(input));
+    time(count: count, name: "Packrat-Quadratic", () => entry.value.packrat.quadratic(input));
+    print("");
+  }
 }
