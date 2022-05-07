cd909a9cc51bdd3bc6cb87b97efc1ba6bd5a08bb
Michaelz35699 <michaelz35699@gmail.com>
2022-05-07 13:46:00 +0800

    Removed `gll` from tests

--- test/util.dart
+++ test/util.dart
@@ -3,37 +3,40 @@
 import "package:parser/parser.dart" as parser;
 import "package:test/test.dart";
 
-Matcher contextSuccess({dynamic index = anything, dynamic result = anything}) => isA<parser.ContextSuccess>()
-    .having((c) => c.state.index, "index", index)
+Matcher contextSuccess({dynamic i = anything, dynamic result = anything}) => isA<parser.ContextSuccess>()
+    .having((c) => c.state.index, "index", i)
     .having((c) => c.mappedResult, "result", result);
-Matcher contextFailure({dynamic index = anything, dynamic message = anything}) => isA<parser.ContextFailure>()
-    .having((c) => c.state.index, "index", index)
+Matcher contextFailure({dynamic i = anything, dynamic message = anything}) => isA<parser.ContextFailure>() //
+    .having((c) => c.state.index, "index", i)
     .having((c) => c.message, "message", message);
-Matcher contextEmpty({dynamic index = anything, dynamic message = anything}) => isA<parser.ContextEmpty>() //
-    .having((c) => c.state.index, "index", index);
+Matcher contextEmpty({dynamic i = anything, dynamic message = anything}) => isA<parser.ContextEmpty>() //
+    .having((c) => c.state.index, "index", i);
 
-Matcher parserSuccess(String input, dynamic res, {dynamic index = anything}) => isA<parser.Parser>()
-    .having((p) => p.pegCtx.pure(input), "peg_pure", contextSuccess(index: index, result: res))
-    .having((p) => p.pegCtx.left(input), "peg_left", contextSuccess(index: index, result: res))
-    .having((p) => p.packratCtx.basic(input), "packrat_basic", contextSuccess(index: index, result: res))
-    .having((p) => p.packratCtx.linear(input), "packrat_linear", contextSuccess(index: index, result: res))
-    .having((p) => p.packratCtx.quadratic(input), "packrat_quadratic", contextSuccess(index: index, result: res))
-    .having((p) => p.gllCtx(input).first, "gll", contextSuccess(index: index, result: res));
+Matcher parserSuccess(String input, dynamic res, {dynamic i = anything}) => isA<parser.Parser>()
+        .having((p) => p.pegCtx.pure(input), "peg_pure", contextSuccess(i: i, result: res))
+        .having((p) => p.pegCtx.left(input), "peg_left", contextSuccess(i: i, result: res))
+        .having((p) => p.packratCtx.basic(input), "packrat_basic", contextSuccess(i: i, result: res))
+        .having((p) => p.packratCtx.linear(input), "packrat_linear", contextSuccess(i: i, result: res))
+        .having((p) => p.packratCtx.quadratic(input), "packrat_quadratic", contextSuccess(i: i, result: res))
+    // .having((p) => p.gllCtx(input).first, "gll", contextSuccess(i: i, result: res))
+    ;
 
-Matcher parserFailure(String input, {dynamic index = anything, dynamic message = anything}) => isA<parser.Parser>()
-    .having((p) => p.pegCtx.pure(input), "peg_pure", contextFailure(index: index, message: message))
-    .having((p) => p.pegCtx.left(input), "peg_left", contextFailure(index: index, message: message))
-    .having((p) => p.packratCtx.basic(input), "packrat_basic", contextFailure(index: index, message: message))
-    .having((p) => p.packratCtx.linear(input), "packrat_linear", contextFailure(index: index, message: message))
-    .having((p) => p.packratCtx.quadratic(input), "packrat_quadratic", contextFailure(index: index, message: message))
-    .having((p) => p.gllCtx(input).first, "gll", contextFailure(index: index, message: message));
+Matcher parserFailure(String input, {dynamic i = anything, dynamic message = anything}) => isA<parser.Parser>()
+        .having((p) => p.pegCtx.pure(input), "peg_pure", contextFailure(i: i, message: message))
+        .having((p) => p.pegCtx.left(input), "peg_left", contextFailure(i: i, message: message))
+        .having((p) => p.packratCtx.basic(input), "packrat_basic", contextFailure(i: i, message: message))
+        .having((p) => p.packratCtx.linear(input), "packrat_linear", contextFailure(i: i, message: message))
+        .having((p) => p.packratCtx.quadratic(input), "packrat_quadratic", contextFailure(i: i, message: message))
+    // .having((p) => p.gllCtx(input).first, "gll", contextFailure(i: i, message: message))
+    ;
 
-Matcher parserEmpty(String input, {dynamic index = anything}) => isA<parser.Parser>()
-    .having((p) => p.pegCtx.pure(input), "peg_pure", contextEmpty(index: index))
-    .having((p) => p.pegCtx.left(input), "peg_left", contextEmpty(index: index))
-    .having((p) => p.packratCtx.basic(input), "packrat_basic", contextEmpty(index: index))
-    .having((p) => p.packratCtx.linear(input), "packrat_linear", contextEmpty(index: index))
-    .having((p) => p.packratCtx.quadratic(input), "packrat_quadratic", contextEmpty(index: index))
-    .having((p) => p.gllCtx(input).first, "gll", contextEmpty(index: index));
+Matcher parserEmpty(String input, {dynamic i = anything}) => isA<parser.Parser>()
+        .having((p) => p.pegCtx.pure(input), "peg_pure", contextEmpty(i: i))
+        .having((p) => p.pegCtx.left(input), "peg_left", contextEmpty(i: i))
+        .having((p) => p.packratCtx.basic(input), "packrat_basic", contextEmpty(i: i))
+        .having((p) => p.packratCtx.linear(input), "packrat_linear", contextEmpty(i: i))
+        .having((p) => p.packratCtx.quadratic(input), "packrat_quadratic", contextEmpty(i: i))
+    // .having((p) => p.gllCtx(input).first, "gll", contextEmpty(i: i))
+    ;
 
 const List<Object?> emptyList = <Object?>[];
