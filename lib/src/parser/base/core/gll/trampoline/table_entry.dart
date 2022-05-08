0429c531cba528c84f66e7e63eb4102809b7069c
Michaelz35699 <michaelz35699@gmail.com>
2022-05-09 02:20:17 +0800

    Changed GllTableEntry to use `Queue`

--- lib/src/parser/base/core/gll/trampoline/table_entry.dart
+++ lib/src/parser/base/core/gll/trampoline/table_entry.dart
@@ -3,10 +3,10 @@ import "dart:collection";
 import "package:parser/internal_all.dart";
 
 class GllTableEntry {
-  final List<GllContinuation> continuations;
+  final Queue<GllContinuation> continuations;
   final HashSet<Context> results;
 
   GllTableEntry()
-      : continuations = <GllContinuation>[],
+      : continuations = Queue<GllContinuation>(),
         results = HashSet<Context>();
 }
